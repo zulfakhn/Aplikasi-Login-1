1b879dd199d2c84efb478a04f8ac062ec4e0937d
zulfakhn <99379315+zulfakhn@users.noreply.github.com>
2022-02-11 15:29:38 +0700

    Update login-script.js
    Mengganti komentar sesuai dengan format JSDoc.

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,21 +1,53 @@
-/* Comment : Membuat variabel untuk setiap elemen view. */
+/** @module Login-Script */
+/** 
+* Membuat variabel loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+
+/** 
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+
+/** 
+* Membuat variabel inputPasswordElement untuk tampilan input password.
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
-/* Comment : Membuat variabel untuk menyimpan informasi email dan password. */
-const expectedEmail = 'admin@dicoding.com';
-const expectedPassword = 'superpassword';
 
+/** 
+ * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
+ * @constant {string}
+ */
+const expectedEmail = 'admin@dicoding.com';
+
+
+/** 
+ * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
+ * @constant {string}
+ */
+const expectedPassword = 'superpassword';
 
 
 /* Comment :  Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function (event) {
   event.preventDefault();
-
-
-/* Comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
+  
+  /** 
+   * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
+   * @constant {string}
+   */
   const email = inputEmailElement.value;
+  
+  /** 
+   * Membuat variabel password untuk menyimpan niali password yang didapatkan saat button ditekan.
+   * @constant {string}
+   */
   const password = inputPasswordElement.value;
 
 
